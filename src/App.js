5de6ea9c651bf700befeae66c3d7439c2b9f7bfc
olacodes <olatundesodiq@gmail.com>
2019-11-21 16:31:57 +0100

    joel testing

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <h1>Hello World!</h1>
       <h2> Is working</h2>
       <h1>Still testing</h1>
+      <h1>Joel Ugumadu said i should deploy for him to see</h1>
     </div>
   );
 }
